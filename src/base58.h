78b40b929f6bbaa87135a4ecbcccd4bcc275eb94
root <root@d1stkfactory>
2014-04-03 13:11:58 +0000

    change wallet address starting letter

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 9, // Applebyte addresses start with A
+        PUBKEY_ADDRESS = 37, // Applebyte addresses start with a
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
