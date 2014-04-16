573ea2387cf2a554f05fda5387e85b1fe45c5723
Bushstar <bushsolo@gmail.com>
2014-04-16 15:29:59 +0100

    Update example addresses

--- src/qt/locale/bitcoin_de.ts
+++ src/qt/locale/bitcoin_de.ts
@@ -1406,8 +1406,8 @@ Adresse: %4</translation>
     </message>
     <message>
         <location line="+34"/>
-        <source>The address to send the payment to (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Die Zahlungsadresse der Überweisung (z.B. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>The address to send the payment to (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Die Zahlungsadresse der Überweisung (z.B. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
     <message>
         <location line="+60"/>
@@ -1447,8 +1447,8 @@ Adresse: %4</translation>
     </message>
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
-        <source>Enter a Applebyte address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Applebyte-Adresse eingeben (z.B. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>Enter a Applebyte address (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Applebyte-Adresse eingeben (z.B. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
 </context>
 <context>
@@ -1470,8 +1470,8 @@ Adresse: %4</translation>
     </message>
     <message>
         <location line="+18"/>
-        <source>The address to sign the message with (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Die Adresse mit der die Nachricht signiert wird (z.B. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>The address to sign the message with (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Die Adresse mit der die Nachricht signiert wird (z.B. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
     <message>
         <location line="+10"/>
@@ -1543,8 +1543,8 @@ Adresse: %4</translation>
     </message>
     <message>
         <location line="+21"/>
-        <source>The address the message was signed with (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Die Adresse mit der die Nachricht signiert wurde (z.B. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>The address the message was signed with (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Die Adresse mit der die Nachricht signiert wurde (z.B. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
     <message>
         <location line="+40"/>
@@ -1564,8 +1564,8 @@ Adresse: %4</translation>
     <message>
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
-        <source>Enter a Applebyte address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Applebyte-Adresse eingeben (z.B. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>Enter a Applebyte address (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Applebyte-Adresse eingeben (z.B. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
     <message>
         <location line="-2"/>
--- src/qt/locale/bitcoin_hi_IN.ts
+++ src/qt/locale/bitcoin_hi_IN.ts
@@ -1403,7 +1403,7 @@ Address: %4
     </message>
     <message>
         <location line="+34"/>
-        <source>The address to send the payment to (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
+        <source>The address to send the payment to (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1444,8 +1444,8 @@ Address: %4
     </message>
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
-        <source>Enter a Applebyte address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Applebyte एड्रेस लिखें (उदाहरण: Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>Enter a Applebyte address (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Applebyte एड्रेस लिखें (उदाहरण: GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
 </context>
 <context>
@@ -1467,7 +1467,7 @@ Address: %4
     </message>
     <message>
         <location line="+18"/>
-        <source>The address to sign the message with (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
+        <source>The address to sign the message with (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1540,7 +1540,7 @@ Address: %4
     </message>
     <message>
         <location line="+21"/>
-        <source>The address the message was signed with (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
+        <source>The address the message was signed with (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1561,8 +1561,8 @@ Address: %4
     <message>
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
-        <source>Enter a Applebyte address (e.g. Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</source>
-        <translation>Applebyte एड्रेस लिखें (उदाहरण: Ler4HNAEfwYhBmGXcFP2Po1NpRUEiK8km2)</translation>
+        <source>Enter a Applebyte address (e.g. GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</source>
+        <translation>Applebyte एड्रेस लिखें (उदाहरण: GBYxGRBdnJ8NEFn21GMKLi7zCsN3P26J3v)</translation>
     </message>
     <message>
         <location line="-2"/>
