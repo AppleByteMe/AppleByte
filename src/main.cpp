c3212da83b77688cb3669923498f3af12b10c5da
Bushstar <bushsolo@gmail.com>
2014-05-10 17:07:04 +0100

    Fix incorrect proof-of-work

--- src/main.cpp
+++ src/main.cpp
@@ -2198,7 +2198,6 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
         pindexPrev = (*mi).second;
         nHeight = pindexPrev->nHeight+1;
 
-        #ifdef _WIN32
         // Check proof of work     
         unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, this);
         double n1 = ConvertBitsToDouble(nBits);
@@ -2206,11 +2205,6 @@ bool CBlock::AcceptBlock(CValidationState &state, CDiskBlockPos *dbp)
 
         if (abs(n1-n2) > n1*0.005) 
             return state.DoS(100, error("AcceptBlock() : incorrect proof of work (DGW2)"));
-        #else
-        // Check proof of work
-        if (nBits != GetNextWorkRequired(pindexPrev, this))
-            return state.DoS(100, error("AcceptBlock() : incorrect proof of work"));
-        #endif
  
         // Check timestamp against prev
         if (GetBlockTime() <= pindexPrev->GetMedianTimePast())
