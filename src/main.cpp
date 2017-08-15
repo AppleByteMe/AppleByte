dc4463043fd225dd3ce0bd3408795430588af064
Peter Bushnell <bushsolo@gmail.com>
2017-08-15 11:24:57 +0100

    Fix incorrect proof-of-work

--- src/main.cpp
+++ src/main.cpp
@@ -3558,19 +3558,13 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
 {
     int nHeight = pindexPrev->nHeight + 1;
 	
-    #ifdef WIN32
-        unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, &block, consensusParams);
-        double n1 = ConvertBitsToDouble(block.nBits);
-        double n2 = ConvertBitsToDouble(nBitsNext);
+    unsigned int nBitsNext = GetNextWorkRequired(pindexPrev, &block, consensusParams);
+    double n1 = ConvertBitsToDouble(block.nBits);
+    double n2 = ConvertBitsToDouble(nBitsNext);
 
-        if (abs(n1-n2) > n1*0.5)
-            return state.DoS(100, error("%s : incorrect proof of work DGW - %f %f %f at %d", __func__, abs(n1-n2), n1, n2, nHeight),
-                            REJECT_INVALID, "bad-diffbits");
-    #else
-        if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
-            return state.DoS(100, error("%s : incorrect proof of work at %d", __func__, nHeight),
-                            REJECT_INVALID, "bad-diffbits");
-    #endif
+    if (abs(n1-n2) > n1*0.5)
+        return state.DoS(100, error("%s : incorrect proof of work DGW - %f %f %f at %d", __func__, abs(n1-n2), n1, n2, nHeight),
+                        REJECT_INVALID, "bad-diffbits");
 	
     // Check proof of work
     if (block.nBits != GetNextWorkRequired(pindexPrev, &block, consensusParams))
