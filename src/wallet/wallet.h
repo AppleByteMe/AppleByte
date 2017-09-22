e34272ad37e962c19ecd64c25a34d8c71be80167
Peter Bushnell <bushsolo@gmail.com>
2017-09-22 11:11:34 +0100

    Increase fallback fee

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -42,7 +42,7 @@ static const unsigned int DEFAULT_KEYPOOL_SIZE = 100;
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const CAmount DEFAULT_FALLBACK_FEE = 200000;
+static const CAmount DEFAULT_FALLBACK_FEE = 2000000;
 //! -mintxfee default
 static const CAmount DEFAULT_TRANSACTION_MINFEE = 100000;
 //! minimum change amount
